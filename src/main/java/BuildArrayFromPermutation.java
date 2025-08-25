2d3e858f399e6c75ab634a327f7552fdfedfc147
rishitha62 <rishithagottiparthy06@gmail.com>
2025-08-25 15:04:29 +0530

    Implement build array

---
+++ src/main/java/BuildArrayFromPermutation.java
@@ -0,0 +1,13 @@
+public class BuildArrayFromPermutation {
+
+    public int[] buildArray(int[] nums) {
+        int n = nums.length;
+        int[] ans = new int[n];
+
+        for(int i = 0; i < n; i++){
+            ans[i] = nums[nums[i]];
+        }
+
+        return ans;
+    }
+}
